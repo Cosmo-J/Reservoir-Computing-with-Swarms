004a4fd4e74dcccb436a5e63b0d1d754c1c29003
Cosmo H <cosmojack@gmail.com>
2026-03-10 15:05:38 +0100

    Began toroidal implemetation. Additionally, fixed bug with parameters not being loaded properly

--- simulator_oop.py
+++ simulator_oop.py
@@ -8,7 +8,7 @@ import argparse
 import os
 import configparser
 from viewer import View as SimViewer
-from scipy.spatial import cKDTree
+from scipy.spatial import KDTree
 import threading
 from concurrent.futures import ThreadPoolExecutor as TPE, as_completed
 
@@ -18,7 +18,7 @@ parser.add_argument('--run',            '-r',   type=str,   nargs='?', const=Tru
 parser.add_argument('--iterations',     '-i',   type=int,   nargs='?', default=1,   help="Use with run or runview to perform multiple simulations.")
 parser.add_argument('--seed',                   type=int,   nargs='?', const=1,     help="Give a random seed to produce identical runs. Default seed is '1'.")
 parser.add_argument('--generate-params','-g',   type=str,   nargs='?', const='./',   help="generate an empty .ini with default parameters.")
-parser.add_argument('--view',           '-v',   type=str,   nargs='+',              help="View a previous simulation, given the file path of a valid '.npz'.")
+parser.add_argument('--view',           '-v',   type=str,   nargs='?', const=True,         help="View a previous simulation, given the file path of a valid '.npz'. Defaults to true, which can be used to view a generated run.")
 parser.add_argument('--save',           '-s',   type=str,   nargs='?', const=False,  help="If iterations is more than 1, this parameter is taken as the save directory. Otherwise")
 parser.add_argument('--multithread',    '-mt',   type=bool, nargs='?', default=False,  const=True, help="set true ")
 
@@ -27,6 +27,8 @@ EMPTY_0x2 = np.empty((0, 2))
 EPS =1e-12 # used for avoiding divide by 0
 DEFAULT_SAVE_PATH = 'tests/boid_runs'
 PARAMS = {}
+COORDINATE_SYSTEMS = ['flat','torus']
+
 
 class SimSaverLoader:
     def __init__(self,save_path='boid_runs',config_title = 'NaN'):
@@ -52,7 +54,6 @@ class SimSaverLoader:
                 raise Exception("Couldn't find a valid name when saving the file. Iterations exceeded number of files in the directory.")
             
         return candidate_path+str(iter)+extension
-
 
     def save_run(self, data, name= None,config_title:str = None) -> str:
         os.makedirs(self.save_dir, exist_ok=True)
@@ -84,10 +85,9 @@ class SimSaverLoader:
             self.save_dir = path
         else:
             raise FileNotFoundError(f"Directory {path} doesn't exist or cant be found.")
-                    
 
-
-    def find_npzs(self, paths:str):
+    @staticmethod
+    def find_npzs(paths:list):
         '''
         Takes some paths, of directories (in which it searches for npzs, or npz paths)
         :return: array of dictionaries containing the npz runs it found
@@ -99,12 +99,13 @@ class SimSaverLoader:
                 npzs.extend([f.path for f in sub_files if '.npz' in f.name])
             else:
                 npzs.append(p)
-        datas =[self.load_run(f) for f in npzs]
+        datas =[SimSaverLoader.load_run(f) for f in npzs]
         print(f'Loaded {len(datas)} run(s)')
 
         return datas
 
-    def load_run(self, path: str) -> dict:
+    @staticmethod
+    def load_run( path: str) -> dict:
         z = np.load(path, allow_pickle=True)
         run_dict = {}
         run_dict["positions"] = z.get("positions"),
@@ -123,7 +124,6 @@ class SimSaverLoader:
                 print(f"\tvalue for '{k}' is missing, features relating to this will not work thus.")
 
         return run_dict
-
 
 class SimParams:
     def __init__(self):
@@ -177,17 +177,20 @@ class SimParams:
                     "L_SAMPLING_RATE" : 0.02 # number of sample per time step, also kind of predator speed
                 }
         }
-        self.params = self.DEFAULTS
+        self.params = self.get_flat_params(self.DEFAULTS)
 
     
-    def get_flat_params(self):
+    def get_flat_params(self,params):
+        '''
+            use to flatten a dict which is storing its parameters two tiered like .ini format. For an example of this two teird thing, see the DEFUALT decleration in this class.
+        '''
         flat = {}
-        for section_title,section in self.DEFAULTS.items():
+        for section_title,section in params.items():
             for k,v in section.items():
                 flat[k] = v
         return flat
     
-    def load_params_from_ini(self,path):
+    def load_params_from_ini(self,path):#also used to load from defaults
         # some sort of path validation to ensure that
         '''
             1. path exists
@@ -195,8 +198,6 @@ class SimParams:
             3. ini is up to date
                 if not, add the new rows and give them the default values
         '''
-
-
         cfg = configparser.ConfigParser()
         cfg.read(path)
         self.params = {}
@@ -206,8 +207,8 @@ class SimParams:
                 if cfg.has_option(section_title,k):
                     self.params[k] = cast(cfg.get(section_title,k))
                 else:
-                    raise Exception(f'Config {path} is missing a key:value for {section_title} {k}\n\tPlease update the .ini to use this config file')
-        
+                    input(f'Config {path} is missing a key:value for {section_title} {k} \n\t Please update the .ini to use this config file!\nType anything to continue with default: {v}')
+                    self.params[k] = v
         return self.params
     
     def write_default_ini(self,path):
@@ -222,8 +223,11 @@ class SimParams:
             cfg.write(f)
 
 class BoidSimulator:
-    def __init__(self):
-        pass
+    def __init__(self,coordinate_system='flat'):
+        if coordinate_system.lower().strip() in COORDINATE_SYSTEMS:
+            self.coord_system = coordinate_system
+        else:
+            raise KeyError(f'Coordinate system "{coordinate_system}" is not a valid option:\n\t{COORDINATE_SYSTEMS}')
 
     def initialise(self,param_loader:SimParams= None, sim_saver_loader:SimSaverLoader=None):
         '''
@@ -235,11 +239,12 @@ class BoidSimulator:
             sim_saver_loader = SimSaverLoader()
 
         self.param_loader = param_loader
-        self.PARAMS = param_loader.get_flat_params()
         self.saver_loader = sim_saver_loader
 
         return param_loader,sim_saver_loader
 
+    def PARAMS(self):
+        return self.param_loader.params
 
     def set_seed(self,seed):
         if isinstance(seed,int): 
@@ -249,6 +254,11 @@ class BoidSimulator:
             np.random.seed(1)
         
 ## Forces
+    def __torus_distance(self, a, b):
+        delta = a - b
+        return delta - np.array(self.sim_size) * np.round(delta / np.array(self.sim_size))
+    
+
     def __repulsion_force(self,boid,neis_x):
         """ 
             boid is an np.array(2) [x,y] of a given boid
@@ -256,7 +266,10 @@ class BoidSimulator:
         """
         if len(neis_x)==0: return np.zeros(2)
 
-        d = boid - neis_x
+        if self.coord_system=='flat':
+            d = boid - neis_x
+        elif self.coord_system=='torus':
+            d = self.__torus_distance(boid,neis_x)
         denom = (d[:,0]**2 + d[:,1]**2) + EPS
         return (d / denom[:,None]).sum(axis=0)
 
@@ -268,51 +281,64 @@ class BoidSimulator:
         return force
 
     def __homing_force(self,boid,home=np.array([0.0,0.0])):
-        return home-boid
+        if self.coord_system=='torus':
+            return self.__torus_distance(home,boid)
+        else:
+            return home-boid
 
     def __friction_force(self,boid_v):
         speed = np.hypot(boid_v[0],boid_v[1])
-        return -boid_v * ((speed - self.PARAMS['K_SPEED']) / self.PARAMS['K_SPEED'])
+        return -boid_v * ((speed - self.PARAMS()['K_SPEED']) / self.PARAMS()['K_SPEED'])
 
     def __predator_force(self,boid_x,pred_x):
         if pred_x is None: return np.array([0.0,0.0])
-        d= np.linalg.norm(boid_x - pred_x)
+
+        if self.coord_system=='torus':
+            numer = self.__torus_distance(boid_x,pred_x)
+        elif self.coord_system=='flat':
+            numer = boid_x - pred_x
+
+        d = np.linalg.norm(numer)
 
         # this 'if else' is the heaviside function
-        if(d<=self.PARAMS['RAD_PREDATOR']):
-            numer = boid_x-pred_x
+        if(d<=self.PARAMS()['RAD_PREDATOR']):
             denom = d**2
-            return (numer/denom)
+            return (numer/denom+EPS)
         else:
             return np.array([0.0,0.0])
 
     def __total_force(self,boid_x,boid_v,a_neighbours,r_neighbours,pred_x=None):
     #           |-coefficent-----------------|-force---------------|-force-params---------|
-        force = ((self.PARAMS['K_ALIGNMENT']*  self.__alignment_force (boid_v,a_neighbours)) +
-                (self.PARAMS['K_REPULSION'] *  self.__repulsion_force (boid_x,r_neighbours)) +
-                (self.PARAMS['K_FRICTION']  *  self.__friction_force  (boid_v)) +
-                (self.PARAMS['K_HOMING']    *  self.__homing_force    (boid_x)) +
-                (self.PARAMS['K_PREDATOR']  *  self.__predator_force  (boid_x,pred_x) ))       
+        force = ((self.PARAMS()['K_ALIGNMENT']*  self.__alignment_force (boid_v,a_neighbours)) +
+                (self.PARAMS()['K_REPULSION'] *  self.__repulsion_force (boid_x,r_neighbours)) +
+                (self.PARAMS()['K_FRICTION']  *  self.__friction_force  (boid_v)) +
+                (self.PARAMS()['K_HOMING']    *  self.__homing_force    (boid_x)) +
+                (self.PARAMS()['K_PREDATOR']  *  self.__predator_force  (boid_x,pred_x) ))       
 
         # sigmoidal function
-        force_sigmoid = self.PARAMS['ALPHA'] * np.tanh(self.PARAMS['BETA'] * force)
+        force_sigmoid = self.PARAMS()['ALPHA'] * np.tanh(self.PARAMS()['BETA'] * force)
 
         return force_sigmoid
 
     def __force_matrix(self,boid_xs,boid_vs,pred_x=None):
         forces = np.empty((len(boid_xs),2))
 
-        tree = cKDTree(boid_xs)
-        align_lists = tree.query_ball_point(boid_xs,r=self.PARAMS['RAD_ALIGNMENT'])
-        repulsion_lists = tree.query_ball_point(boid_xs,r=self.PARAMS['RAD_REPULSION'])
+        if self.coord_system == 'flat':
+            tree_points = boid_xs
+            tree = KDTree(tree_points)
+
+        elif self.coord_system == 'torus':
+            tree_points = self.__flat_to_torus(boid_xs)
+            tree = KDTree(tree_points, boxsize=self.sim_size)
+            
+        align_lists = tree.query_ball_point(tree_points,r=self.PARAMS()['RAD_ALIGNMENT'])
+        repulsion_lists = tree.query_ball_point(tree_points,r=self.PARAMS()['RAD_REPULSION'])
 
         for i, (x,v) in enumerate(zip(boid_xs,boid_vs)):
             a_idx = [j for j in align_lists[i] if j != i]
             r_idx = [j for j in repulsion_lists[i] if j != i]
-
             a_neis_v = boid_vs[a_idx] if a_idx else EMPTY_0x2
             r_neis_x = boid_xs[r_idx] if r_idx else EMPTY_0x2
-
             forces[i] = self.__total_force(x,v,a_neis_v,r_neis_x,pred_x)
 
 
@@ -320,9 +346,9 @@ class BoidSimulator:
 
     def __lorenz_equations(self,t,start_states):
         x,y,z = start_states
-        dxBYdt = self.PARAMS['L_SIGMA'] * (y-x)
-        dyBYdt = x * (self.PARAMS['L_RHO'] - z) - y
-        dzBYdt = (x * y) - (self.PARAMS['L_BETA'] * z)
+        dxBYdt = self.PARAMS()['L_SIGMA'] * (y-x)
+        dyBYdt = x * (self.PARAMS()['L_RHO'] - z) - y
+        dzBYdt = (x * y) - (self.PARAMS()['L_BETA'] * z)
         return dxBYdt,dyBYdt,dzBYdt
 
     def __generate_lorenz(self,time_steps, sample_rate, x_init, y_init, z_init):
@@ -350,65 +376,73 @@ class BoidSimulator:
         # returns positions, velocities, neighbour data
         return x, v
 
-    def __evolve(self,t,positions,velocities,prior_lorenz_x):
+    def __flat_to_torus(self,positions):
+        return (positions + self.sim_size[0]) % np.array(self.sim_size)
+    
+    def __physics_step(self,t,positions,velocities,prior_lorenz_x):
         '''
         the priors are the given parameter at t
         '''
         current_x = positions[t]
         current_v = velocities[t]
 
-        #2. Create force matrix
+        #2. Calculate the force matrix (forces acting on each boid) at the current step
         fm = self.__force_matrix(current_x,current_v,prior_lorenz_x)
 
         #3. update velocity matrix
         #4. update position matrix
-        new_v = current_v + (fm * self.PARAMS['DELTA_T'])
-        new_x = current_x + (new_v * self.PARAMS['DELTA_T'])
+        new_v = current_v + (fm * self.PARAMS()['DELTA_T'])
+        new_x = current_x + (new_v * self.PARAMS()['DELTA_T'])
 
         return new_v,new_x
-
+    
     def run_simulation(self):
-        if len(self.PARAMS) is None:
+        if len(self.PARAMS()) is None:
             raise Exception('Please generate params and apply them before running simulation. apply_params()')
-
 
         positions = []
         velocities = []
 
-        lorenz = self.__generate_lorenz(self.PARAMS['TIME_STEPS'], self.PARAMS['L_SAMPLING_RATE'], self.PARAMS['X_LORENZ'], self.PARAMS['Y_LORENZ'], self.PARAMS['Z_LORENZ'])
+        lorenz = self.__generate_lorenz(self.PARAMS()['TIME_STEPS'], self.PARAMS()['L_SAMPLING_RATE'], self.PARAMS()['X_LORENZ'], self.PARAMS()['Y_LORENZ'], self.PARAMS()['Z_LORENZ'])
 
         #nts repeatedly dry running the oop version to find and work through bugs, currently trying to fix spawnbounds which was originally 'tupliised' in the get params in the original simulator fuckers
-        spawn_bounds = (self.PARAMS['SPAWN_MIN'],self.PARAMS['SPAWN_MAX'])
-        p, v = self.__generate_flock(self.PARAMS['BOID_COUNT'], spawn_bounds, self.PARAMS['RANDOM_VELOCITY'])
+        self.spawn_bounds = (self.PARAMS()['SPAWN_MIN'],self.PARAMS()['SPAWN_MAX'])
+        WH = (self.PARAMS()['SPAWN_MAX'] - self.PARAMS()['SPAWN_MIN']) * 10
+        self.sim_size = (WH,WH)
+
+        p, v = self.__generate_flock(self.PARAMS()['BOID_COUNT'], self.spawn_bounds, self.PARAMS()['RANDOM_VELOCITY'])
         positions.append(p)
         velocities.append(v)
 
-
-        # below is a very excentric way of getting the number from the end of the thread name seen <Thread(ThreadPoolExecutor-0_0, started 6119583744)> (which is what current_thread() returns in a MT scenario)
-        # otherwise rely on the failure to make the letter d an int to state that its a single threading scenario and thread indent should be 0 LOL
-        # not too worried about the bad practise here considering the code is purely for aesthetics
-        try: 
+        """below is a very excentric way of getting the number from the end of the thread name seen <Thread(ThreadPoolExecutor-0_0, started 6119583744)> (which is what current_thread() returns in a MT scenario)
+        otherwise rely on the failure to make the letter d an int to state that its a single threading scenario and thread indent should be 0 LOL
+        not too worried about the bad practise here considering the code is purely for aesthetics"""
+        try:
             thread_indent = int(threading.current_thread().name[-1])+1
         except:
             thread_indent=0
 
-        for t in trange(self.PARAMS['TIME_STEPS'] - 1,desc=f"Thread: {threading.current_thread().name}",position=thread_indent,leave=False):
-            new_v, new_x = self.__evolve(t,positions, velocities, lorenz[t])
+        for t in trange(self.PARAMS()['TIME_STEPS'] - 1,desc=f"Thread: {threading.current_thread().name}",position=thread_indent,leave=False):
+            new_v, new_x = self.__physics_step(t,positions, velocities, lorenz[t])
+            if self.coord_system == 'torus':
+                new_x = new_x % np.array(self.sim_size)
+
             positions.append(new_x)
             velocities.append(new_v)
+
+
+        if self.coord_system=='torus':
+            positions = np.array(positions)-self.sim_size[0]/2
 
         return {
             "positions": positions,
             "velocities": velocities,
             "predator_positions": lorenz,
-            "time_steps": self.PARAMS['TIME_STEPS'],
-            "boid_count": self.PARAMS['BOID_COUNT'],
-            "bounds": spawn_bounds,
+            "time_steps": self.PARAMS()['TIME_STEPS'],
+            "boid_count": self.PARAMS()['BOID_COUNT'],
+            "bounds": self.spawn_bounds,
         }
 
-#----------------------------------------------------------------
-
-#----------------------------------------------------------------
 def main(custom_args=None):
     os.makedirs(DEFAULT_SAVE_PATH, exist_ok=True)
     #below accounts for main being called by other scripts, where custom args is a custom object
@@ -462,7 +496,6 @@ def main(custom_args=None):
         # setting the seed
         Simulator.set_seed(seed)
 
-
         if multithread:
             datas = [None] * iterations
             executor = TPE(max_workers=4)
@@ -473,7 +506,7 @@ def main(custom_args=None):
                 datas[i] = result
                 
                 if we_be_saving: SaverLoader.save_run(result,config_title=run)
-                executor.shutdown()
+            executor.shutdown()
         else:
             datas=[]
             for i in trange(iterations,desc='Iterations',position=0):
