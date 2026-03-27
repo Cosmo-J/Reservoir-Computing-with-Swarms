3d1beee64660ad73bacdf525637d14a32ea51ece
Cosmo H <cosmojack@gmail.com>
2026-03-27 12:15:53 +0000

    forgot to remove propartyanalsysis import from src/__init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,7 @@
 from .BoidSimulator import BoidSimulator
 from .BoidVisualizer import BoidVisualizer
-from .PropertyAnalysis import PropertyAnalysis
 from .SimParams import SimParams
 from .SimSaverLoader import SimSaverLoader
 from .ObservationLayer import KernelReadout,FlatReadout
 
-__all__ = [BoidSimulator,BoidVisualizer,PropertyAnalysis,SimParams,SimSaverLoader,KernelReadout,FlatReadout]
+__all__ = [BoidSimulator,BoidVisualizer,SimParams,SimSaverLoader,KernelReadout,FlatReadout]
