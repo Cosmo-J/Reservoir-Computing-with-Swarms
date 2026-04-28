84ab18cea9ed157a5cda6705743d3c80c5ad8b52
Cosmo H <cosmojack@gmail.com>
2026-04-28 21:15:57 +0100

    Renamed from ObservationLayer to new name to bring in line with descriptions made in the dissertation.

--- src/__init__.py
+++ src/__init__.py
@@ -2,4 +2,4 @@ from .Simulators import BoidSimulator, LorenzSimulator
 from .Visualisers import flat_render,torus_render, plot_ridge_predictions,plot_consistency_profile
 from .ConfigManager import compare_params,generate_config,load_config
 from .SaverLoader import Saver, create_mmap, load_run, load_pca, load_prediction, load_readout, load_npzs, npy_cleanup
-from .ObservationLayer import ReadoutMethod, KernelReadout, NaiveReadout, COMReadout, NoReadout
+from .ObservationAnalysis import ReadoutMethod, KernelReadout, NaiveReadout, COMReadout, NoReadout
